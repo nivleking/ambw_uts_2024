5d854d2c507e4121d88dac27d31cd69224af776f
Kelvin Sidharta Sie <118882421+nivleking@users.noreply.github.com>
2024-04-28 15:48:21 +0700

    fix: overflow intro page

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -8,6 +8,7 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topRight,
@@ -18,70 +19,74 @@ class IntroPage extends StatelessWidget {
             ],
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(48.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                height: 200,
-                width: 200,
-                color: Colors.white,
-              ),
-              const SizedBox(
-                height: 48,
-              ),
-              const Text(
-                "Welcome to Sydney CBD",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                "Find delicous and price-friendly meals and foods in here!",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 48,
-              ),
-              GestureDetector(
-                onTap: () {
-                  // print("1234567890");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const StartPage(),
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(12),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(48.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 200,
+                    width: 200,
+                    color: Colors.white,
                   ),
-                  padding: const EdgeInsets.all(25),
-                  child: const Center(
-                    child: Text(
-                      "Order Now",
-                      style: TextStyle(
-                        color: Colors.white,
+                  const SizedBox(
+                    height: 48,
+                  ),
+                  const Text(
+                    "Welcome to Sydney CBD",
+                    style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 16,
+                        fontSize: 20,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  const Text(
+                    "Find delicous and price-friendly meals and foods in here!",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 48,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // print("1234567890");
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const StartPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.all(25),
+                      child: const Center(
+                        child: Text(
+                          "Order Now",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                ],
+              ),
+            ),
           ),
         ),
       ),
